5b9399ac9b12dc690b7929fdea89215735ada3b0
Armaklan <lionel.zuber@armaklan.org>
2014-11-15 23:28:14 +0100

    feat(boardsList) - List all board and prompt for choice
    List board for user
    Prompt for board to extract

--- app.js
+++ app.js
@@ -1,24 +1,42 @@
 /*
  * app.js
- * Copyright (C) 2014 armaklan <armaklan@dracoport>
+ * Copyright (C) 2014 armaklan <lionel.zuber@armaklan.org>
  *
  * Distributed under terms of the MIT license.
  */
 var config = require('./config.js');
+var prompt = require('prompt');
 var Trello = require("node-trello");
 var t = new Trello(config.key, config.token);
 
-// t.get("/1/members/me", function(err, data) {
-//       if (err) throw err;
-//         console.log(data);
-// });
+var promptProperties = [{
+    name: 'boardIndex',
+    validator: /^\d+$/
+}];
 
-t.get('1/boards/545340c9b9648624872ab763/lists?cards=open', function(err, data) {
-    if(err) throw err;
-    data.forEach(function(column) {
-        console.log(column.name);
-        column.cards.forEach(function(card) {
-            console.log('   ' + card.name);
+var getBoardsCard = function(board) {
+    t.get('1/boards/' + board.id + '/lists?cards=open', function(err, data) {
+        if(err) throw err;
+        data.forEach(function(column) {
+            console.log(column.name);
+            column.cards.forEach(function(card) {
+                console.log('   ' + card.name);
+            });
         });
     });
+};
+
+t.get('1/members/me?boards=open', function(err, data) {
+    var boards = [];
+    data.boards.forEach(function(board) {
+        console.log(boards.length + " - " + board.name);
+        boards.push(board);
+    });
+    console.log("Quel tableau de bord voulez-vous extraire ? ");
+    prompt.start();
+    prompt.get(promptProperties, function(err, result){
+        getBoardsCard(boards[result.boardIndex]);
+    });
 });
+
+
